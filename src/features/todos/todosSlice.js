316d057f607db15d94972fa1f59ee4117dfd3798
Hexact task for Junior React JS <sevan.keshish0@gmail.com>
2023-08-26 23:33:56 +0400

    todosSlice

---
+++ src/features/todos/todosSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [
+  { id: 1, title: "todo1", completed: false },
+  { id: 2, title: "todo2", completed: false },
+  { id: 3, title: "todo3", completed: true },
+  { id: 4, title: "todo4", completed: false },
+  { id: 5, title: "todo5", completed: false },
+];
+
+const todosSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const newTodo = {
+        id: Date.now(),
+        title: action.payload.title,
+        completed: false,
+      };
+      state.push(newTodo);
+    },
+  },
+});
+
+export const { addTodo } = todosSlice.actions;
+export default todosSlice.reducer;
